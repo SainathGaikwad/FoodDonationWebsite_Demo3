89d652fe22f88c3138371ab73c37a873745c694a
Sainath Atmaram Gaikwad <89898283+SainathGaikwad@users.noreply.github.com>
2023-12-14 15:00:10 +0530

    admin panel changes

--- includes/header.php
+++ includes/header.php
@@ -4,10 +4,10 @@
             <span class="menu"><img src="images/loga.jpg" alt="" /></span>
             <ul class="nav1">
                 <li><a href="index.php">HOME</a></li>
-                <li><a href="about.php">ABOUT</a></li>
+                <li><a href="about.php">ABOUT US</a></li>
                 <li><a href="food-available.php"> AVAILABLE FOOD LIST</a></li>
 
-                <li><a href="contact.php">CONTACT</a></li>
+                <li><a href="contact.php">CONTACT US</a></li>
                 <li><a href="donor/login.php">DONOR</a></li>
                 <li><a href="admin/login.php">ADMIN</a></li>
                 <li><a href="campaign.php">OUR CAMPAIGN</a></li>
@@ -15,9 +15,9 @@
 
             </ul>
             <script>
-            $("span.menu").click(function() {
-                $("ul.nav1").slideToggle(300, function() {});
-            });
+                $("span.menu").click(function() {
+                    $("ul.nav1").slideToggle(300, function() {});
+                });
             </script>
         </div>
 
